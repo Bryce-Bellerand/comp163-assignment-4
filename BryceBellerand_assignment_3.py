42fb696f943d2324d740b9d472a5ac92bd352502
Bryce-Bellerand <brycebellerand@gmail.com>
2025-09-26 19:53:16 -0400

    git add BryceBellerand_assignment_3.py 

---
+++ BryceBellerand_assignment_3.py
@@ -0,0 +1,11 @@
+student_name = "Bryce Bellerand"
+current_gpa = 3.2 #float betweeen 1.0 - 4.0
+study_hours = int(5) #integer
+social_points = int(32)
+stress_level = 75 #integer 1-100
+passing = None
+classes = ["Programming", "Calculus", "English"]
+#Display welcome stats
+
+print(f"Welcome, {student_name}!")
+print(f"GPA: {current_gpa}, Study Hours: {study_hours}, Social Points: {social_points}, Stress Level: {stress_level}")
